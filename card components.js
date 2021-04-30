17d64b518a091d6ab87813da5c2acfa7174f0306
Reuven <reuvenroth@users.noreply.github.com>
2021-04-30 02:35:55 -0400

    Update card components.js
    Input feature added. Includes standard input bar, Add card button, and GitHub username placeholder.
    
    The current profile data array driving the code is currently positioned as a global variable. This will be updated shortly.

--- card components.js
+++ card components.js
@@ -1,18 +1,18 @@
-	const testData = [
-			{name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
-      {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
-  		{name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
-	];
+const testData = [
+    {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
+    {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+    {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
+];
 
 const CardList = (props) => (
-  <div>
-    {testData.map(profile => <Card {...profile}/>)}
-  </div>
+	<div>
+  	{testData.map(profile => <Card {...profile}/>)}
+	</div>
 );
 
 class Card extends React.Component {
 	render() {
-    const profile = this.props;
+  	const profile = this.props;
   	return (
     	<div className="github-profile">
     	  <img src={profile.avatar_url} />
@@ -25,11 +25,22 @@ class Card extends React.Component {
   }
 }
 
+class Form extends React.Component {
+  render() {
+    return (
+      <form action="">
+        <input type="text" placeholder="GitHub username"/>
+        <button>Add card</button>
+      </form>
+    );
+  }
+}
 class App extends React.Component {
 	render() {
   	return (
     	<div>
     	  <div className="header">{this.props.title}</div>
+        <Form />
         <CardList />
     	</div>
     );
