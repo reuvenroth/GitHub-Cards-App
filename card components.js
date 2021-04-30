cd59cedefbbcfddc6ea90a3b27b3cf7ee15705c0
Reuven <reuvenroth@users.noreply.github.com>
2021-04-30 03:17:18 -0400

    Update card components.js
    Changed from ref{} method to state/setState method. An onChange component was added to the Form class to allow React/console to be aware of the string input characters as they're typed. This can be used to suggest stronger passwords. This will likely be useful in the Tanach/Gematria search project.

--- card components.js
+++ card components.js
@@ -26,20 +26,19 @@ class Card extends React.Component {
 }
 
 class Form extends React.Component {
-  userNameInput = React.createRef();
+  state = { userName: '' };
   handleSubmit = (event) => {
     event.preventDefault();
-    console.log(
-    this.userNameInput.current.value
-    )
+    console.log(this.state.userName);
   };
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <input 
           type="text" 
+          value={this.state.userName}
+          onChange={event => this.setState({ userName: event.target.value })}
           placeholder="GitHub username" 
-          ref={this.userNameInput}
           required />
         <button>Add card</button>
       </form>
