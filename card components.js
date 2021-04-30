cab3b1149000f5c17ff967e73ff2dbd69f403d3f
Reuven <reuvenroth@users.noreply.github.com>
2021-04-30 03:07:23 -0400

    Update card components.js
    Added "ref" attribute using React.createRef(); API method. Controlled ref components coming shortly.

--- card components.js
+++ card components.js
@@ -26,13 +26,21 @@ class Card extends React.Component {
 }
 
 class Form extends React.Component {
+  userNameInput = React.createRef();
   handleSubmit = (event) => {
     event.preventDefault();
+    console.log(
+    this.userNameInput.current.value
+    )
   };
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <input type="text" placeholder="GitHub username" required />
+        <input 
+          type="text" 
+          placeholder="GitHub username" 
+          ref={this.userNameInput}
+          required />
         <button>Add card</button>
       </form>
     );
